9cebbbc7857c9cb235fc4c7358145e34dd0650c9
yuchimiri <yuchimiri24@gmail.com>
2013-02-04 20:37:01 +0900

    Added validation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :created_at, :name
+  validates :name, :presence => true
+  validates :content, :presence => true,
+                      :length => { :maximum => 1000 }
 end
